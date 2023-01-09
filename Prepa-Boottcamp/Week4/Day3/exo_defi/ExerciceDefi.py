65235de9ca2e14bd53ff99cc9a149a48627892eb
kalmogopatrick <patrickkalmogo55@gmail.com>
2023-01-10 00:16:20 +0100

    day3

--- Prepa-Boottcamp/Week4/Day3/exo_defi/ExerciceDefi.py
+++ Prepa-Boottcamp/Week4/Day3/exo_defi/ExerciceDefi.py
@@ -1,14 +1,19 @@
-"""
+
 #Defi 1
+import re
 word = input("Entrer votre mots:")
+worde=re.sub(r"\s+", "" , word)
+print(worde)
 words = {}
-for i in range(len(word)):
-    mots = word[i]
+for i in range(len(worde)):
+    mots = worde[i]
     if mots in words:
         words[mots].append(i)
     else:
         words[mots]=[i]
-print(words)"""
+print(words)
+
+"""
 #defi2
 Articles = {"TV":"$100", "Phone":"$10", "table":"$5", "Water":"$200", "sac":"$1.1", "livre":"$5.2", "vélo":"$800"}
 liste =[]
@@ -20,7 +25,7 @@ if liste:
     liste.sort()
     print("Voici la liste des articles dont vous pouvez payer\n:",liste)
 else:
-    print("nothing!")
+    print("nothing!")"""
 
 
 
---
+++ Prepa-Boottcamp/Week4/Day4/Exo_xp/ExerciceXP.py
@@ -0,0 +1,25 @@
+#Exercice2
+
+"""def favorite_book(users):
+    print(" Un de mes livres préférés est " + users.title())
+titres =input("titre du livre:")
+favorite_book(titres)"""
+
+#Exercice3
+"""
+def country(ville,pays):
+    print(ville.title(),"est la ville du", pays.title())
+vil= input("nom de la ville:")
+pay = input("nom du pays:")
+country(vil,pay)"""
+
+
+#Exercice5
+def make_shirt(taille,texte):
+    print("la taille de ma chemise est ",taille.title(),"et l'écriture est",texte.title())
+tail =input("Taille de la cemise:")
+ecrit = input("Texte sur la chemise :")
+make_shirt(tail,ecrit)
+
+
+    
